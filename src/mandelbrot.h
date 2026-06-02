bc12a8511bfd77986d8920ea52cddac3becf8c50
William Conner <redwils5@gmail.com>
2026-06-02 14:24:47 -0400

    Color rendering

--- src/mandelbrot.h
+++ src/mandelbrot.h
@@ -6,15 +6,15 @@
  * @param dy The imaginary component of c 
  * @return The number of iterations it took for z_n to determine z_n will diverge to infinity
  */
-unsigned int mandelbrot(float dx, float dy, unsigned int max_iterations) {
+unsigned int mandelbrot(double dx, double dy, unsigned int max_iterations) {
     // z_0 = 0
-    float x = 0; // Real component of z_n
-    float y = 0; // Imaginary component of z_n
+    double x = 0; // Real component of z_n
+    double y = 0; // Imaginary component of z_n
     
     unsigned int iterations = 0;
     while (x*x + y*y <= 2*2 && iterations < max_iterations) {
         // z_n = z_{n-1}^2 + c
-        float x_next = x*x - y*y + dx;
+        double x_next = x*x - y*y + dx;
         y = 2*x*y + dy;
         x = x_next;
         
--- src/naive.c
+++ src/naive.c
@@ -1,23 +1,24 @@
 #include "mandelbrot.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define MAX_ITERATIONS 255
 
 int main(int argc, char *argv[]) {
-    if (argc != 8) {
+    if (argc != 9) {
         fprintf(stderr, "Expected 7 arguments, received %d\n", argc - 1);
         return EXIT_FAILURE;
     }
 
     char filename[64];
     sprintf(filename, "%s.ppm", argv[1]);
-    int width   = atoi(argv[2]);
-    int height  = atoi(argv[3]);
-    float x_max = atof(argv[4]);
-    float x_min = atof(argv[5]);
-    float y_max = atof(argv[6]);
-    float y_min = atof(argv[7]);
+    int width    = atoi(argv[2]);
+    int height   = atoi(argv[3]);
+    double x_max = atof(argv[4]);
+    double x_min = atof(argv[5]);
+    double y_max = atof(argv[6]);
+    double y_min = atof(argv[7]);
 
     FILE *fp = fopen(filename, "wb");
     if (!fp) {
@@ -32,14 +33,36 @@ int main(int argc, char *argv[]) {
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             int i = mandelbrot(
-                (x / (float)height) * (x_max - x_min) + x_min,
-                (y / (float)height) * (y_max - y_min) + y_min,
+                (x / (double)height) * (x_max - x_min) + x_min,
+                (y / (double)height) * (y_max - y_min) + y_min,
                 MAX_ITERATIONS
             );
             int is_in = i == MAX_ITERATIONS;
-            unsigned char r = 0;
-            unsigned char g = 0;
-            unsigned char b = is_in ? 0 : 128;
+            int r, g, b;
+            if (is_in) {
+                r = 0;
+                g = 0;
+                b = 0;
+            } else {
+                float hue = i / (float)MAX_ITERATIONS;
+
+                // Convert HSV (S & V = 100%) to RGB
+                float h = hue * 6.0;
+                int j = floor(h);
+                float f = h - j;
+                
+                float q = 1.0 - f;
+                float t = f;
+
+                switch (j % 6) {
+                    case 0: r = 255,    g = t*255,  b = 0;      break;
+                    case 1: r = q*255,  g = 255,    b = 0;      break;
+                    case 2: r = 0,      g = 255,    b = t*255;  break;
+                    case 3: r = 0,      g = q*255,  b = 255;    break;
+                    case 4: r = t*255,  g = 0,      b = 255;    break;
+                    case 5: r = 255,    g = 0,      b = q*255;  break;
+                }
+            }
 
             fputc(r, fp);
             fputc(g, fp);
