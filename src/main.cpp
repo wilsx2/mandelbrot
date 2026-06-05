f87363542db8355462a41ac3996a1924ac4793a8
William Conner <redwils5@gmail.com>
2026-06-05 14:40:33 -0400

    Brought in Argumentum for argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include "mplot/mplot.h"
+#include "mplot/mplot.hpp"
+#include "argumentum/argparse.h"
 #include <print>
 #include <format>
 #include <string>
@@ -8,23 +9,51 @@
 constexpr unsigned int MAX_ITERATIONS = 255;
 
 int main(int argc, char *argv[]) {
-    if (argc != 8) {
-        std::println(stderr, "Expected 7 arguments, received {}", argc - 1);
+    // Parse arguments
+    auto parser = argumentum::argument_parser{};
+    auto params = parser.params();
+
+    if (argc < 1) {
         std::exit(EXIT_FAILURE);
     }
+    parser.config().program(argv[0]).description("Mandelbrot Set Plotter");
 
-    // Parse arguments
-    std::string filename = std::format("{}.ppm", std::string_view(argv[1]));
-    std::size_t width    = std::stoull(argv[2]);
-    std::size_t height   = std::stoull(argv[3]);
-    double real_max      = std::stod(argv[4]);
-    double real_min      = std::stod(argv[5]);
-    double imag_max      = std::stod(argv[6]);
-    double imag_min      = std::stod(argv[7]);
+    std::string filepath;
+    params.add_parameter(filepath, "--output", "-o")
+          .nargs(1)
+          .absent("output.ppm")
+          .help("Path to the output file (.ppm format) (default: output.ppm)");
+
+    std::array<int, 2> dimensions;
+    params.add_parameter(dimensions, "--dimensions", "-d")
+          .nargs(2)
+          .absent({1920,1080})
+          .help("Width and height of output image (default: 1920 1080)");
+
+    std::array<double, 2> real_limits;
+    params.add_parameter(real_limits, "--real-limits", "-r")
+          .nargs(2)
+          .absent({-2.0, 0.5})
+          .help("Minimum and maximum values of the real component of 'c' to be graphed (default: -2.0 0.5)");
+
+    std::array<double, 2> imag_limits;
+    params.add_parameter(imag_limits, "--imaginary-limits", "-i")
+          .nargs(2)
+          .absent({-1.25, 1.25})
+          .help("Minimum and maximum values of the imaginary component of 'c' to be graphed (default: -1.25 1.25)");
+
+    if (!parser.parse_args(argc, argv)) {
+        std::exit(EXIT_FAILURE);
+    }
+    
+    std::println("Rendering plot to \"{}\"", filepath);
+    std::println("\tDimensions: {}x{}", dimensions[0], dimensions[1]);
+    std::println("\tReal Limits: min {}, max {}", real_limits[0], real_limits[1]);
+    std::println("\tImaginary Limits: min {}, max {}", imag_limits[0], imag_limits[1]);
 
     // Plot and render
-    mplot::axis_limits lim {{real_min, imag_min}, {real_max, imag_max}};
-    bool success = mplot::save_to_ppm(filename, width, height, lim,
+    mplot::axis_limits lim {{real_limits[0], imag_limits[0]}, {real_limits[1], imag_limits[1]}};
+    bool success = mplot::save_to_ppm(filepath, dimensions[0], dimensions[1], lim,
         [](std::complex<double> c) -> mplot::pixel {
             auto percent = mplot::escape_time_percent(c, MAX_ITERATIONS); 
             if (percent == 1.0)
