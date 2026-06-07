7dcab7ce5c3e93e856ea59b7bcac8e5ba708d705
William Conner <redwils5@gmail.com>
2026-06-07 17:15:30 -0400

    Extended Benchmark with Args

--- src/bench.cpp
+++ src/bench.cpp
@@ -1,18 +1,20 @@
 #include <wacfrac/wacfrac.hpp>
 #include <benchmark/benchmark.h>
 
-static void bm_render_standard(benchmark::State& state) {
-    wacfrac::multi_complex::default_precision(10); // Note: Magic
-    wacfrac::multi_float::default_precision(10); // Note: Magic
+static void bm_render(benchmark::State& state) {
+    wacfrac::multi_complex::default_precision(state.range(0));
+    wacfrac::multi_float::default_precision(state.range(0));
 
     wacfrac::plot plot {
-        wacfrac::video_resolution::SD360p,
+        {100uz, 100uz}, // Note: Magic
         wacfrac::FULL_SET,
-        64 // Note: Magic
+        static_cast<std::size_t>(state.range(1))
     };
     std::vector<wacfrac::pixel> buffer (plot.res.width * plot.res.height);
     for (auto _ : state)
         plot.render(buffer);
 }
-BENCHMARK(bm_render_standard);
+BENCHMARK(bm_render)
+    ->Args({5, 32});
+
 BENCHMARK_MAIN();
