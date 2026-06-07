0bbf0fe267dec77e38e9636ff59a9cdb05665254
William Conner <redwils5@gmail.com>
2026-06-07 19:38:53 -0400

    Wrote C++20 concept for Complex type

--- include/wacfrac/fractal.hpp
+++ include/wacfrac/fractal.hpp
@@ -1,12 +1,21 @@
 #pragma once
 
+#include <concepts>
 #include <wacfrac/types.hpp>
 
 namespace wacfrac
 {
 
-template<typename T>
-    // TODO: Concepts
+template <typename T>
+concept Complex = requires(T a, T b) {
+    { static_cast<double>(a.real()) } -> std::same_as<double>;
+    { static_cast<double>(a.imag()) } -> std::same_as<double>;
+    { a + b } -> std::convertible_to<T>;
+    { a * b } -> std::convertible_to<T>;
+    T{0.0, 0.0};
+};
+
+template<Complex T>
 auto escape_time(T c, unsigned int max_iterations) -> unsigned int {
     T z {0.0, 0.0};
     auto n = 0u;
