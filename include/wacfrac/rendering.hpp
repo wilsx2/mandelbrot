2e6938dea1ad3601eecf1a46cca9aacf68e37e7e
William Conner <redwils5@gmail.com>
2026-06-23 15:41:12 -0400

    CPU parallelism by pixel

--- include/wacfrac/rendering.hpp
+++ include/wacfrac/rendering.hpp
@@ -4,6 +4,7 @@
 #include <wacfrac/orbit.hpp>
 #include <wacfrac/viewport.hpp>
 #include <wacfrac/resolution.hpp>
+#include <execution>
 #include <span>
 #include <cstddef>
 
@@ -12,13 +13,13 @@ namespace wacfrac
 
 template <std::invocable<std::size_t, std::size_t> F, typename G>
 void screen_render(std::span<pixel> pixels, const resolution& res, F&& escape_fn, G&& color_fn) {
-    auto i {0uz};
-
-    for (auto [y, x] : res.coordinates()) {
+    auto coords {res.coordinates()};
+    std::for_each(std::execution::par, coords.begin(), coords.end(),[&](auto&& coord){
+        auto [y, x] = coord;
         auto result = escape_fn(x, y);
 
         std::size_t n {std::get<1>(result)};
-        pixels[i++] = [&]() {
+        pixels[x + y * res.width] = [&]() {
             if constexpr (std::invocable<G, std::complex<float>, decltype(n)>) {
                 std::complex<float> z {std::get<0>(result)};
                 return color_fn(z, n);
@@ -28,7 +29,7 @@ void screen_render(std::span<pixel> pixels, const resolution& res, F&& escape_fn
                 static_assert(false, "color_fn does not accept (z, n) or (n)");
             }
         }();
-    }
+    });
 }
 
 template <Complex T, std::invocable<T> F, typename G>
