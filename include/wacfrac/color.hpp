477a068d5beba172f3bdaaca714b40c1547412c0
William Conner <redwils5@gmail.com>
2026-07-03 07:24:43 -0400

    Removed more unused code

--- include/wacfrac/color.hpp
+++ include/wacfrac/color.hpp
@@ -36,22 +36,7 @@ inline const std::vector<Pixel> ULTRA {
 
 auto parse_color(std::string_view filename) -> Pixel;
 
-// colorization
-// https://www.tomchaplin.xyz/blog/2018-11-02-exploring-the-mandelbrot-set/
-// https://linas.org/art-gallery/escape/escape.html
 auto colorize_discrete(const std::vector<Pixel>& palette, std::size_t max_n, std::size_t n) -> Pixel;
 auto colorize_continuous(const std::vector<Pixel>& palette, std::size_t max_n, std::complex<float> z, std::size_t n) -> Pixel;
-
-// manipulation
-auto lerp_pixel(Pixel a, Pixel b, float percentile) -> Pixel;
-auto lerp_color(Color a, Color b, float percentile) -> Color;
-auto to_pixel(ColorEncoding e, Color c) -> Pixel;
-auto rgb_to_pixel(float r, float g, float b) -> Pixel;
-auto hsv_to_pixel(float h, float s, float v) -> Pixel;
-auto hcl_to_pixel(float l, float c, float h) -> Pixel;
-
-// palette generation
-auto generate_palette(std::size_t size, std::initializer_list<Pixel> samples) -> std::vector<Pixel>;
-auto generate_palette(std::size_t size, ColorEncoding encoding, std::initializer_list<Color> samples) -> std::vector<Pixel>;
 
 } // namespace wacfrac
