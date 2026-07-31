337ba0178f14560706bcac79e7eb39ce43fabb42
William Conner <redwils5@gmail.com>
2026-07-31 07:50:34 -0400

    Reorganized and renamed examples directory -> scripts

--- scripts/examples.bash
+++ scripts/examples.bash
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 do_render() {
-    local output_file="renders/$1"
+    local output_file="output/$1"
     if [ ! -f "$output_file.png" ]; then
-        ../build/wacfrac -c "$(bash colors/$2.sh $3)" -f $(cat poi/$4) -r 3840 2160 -N 250 50 2 image -z $5 -o $output_file.ppm
+        ./build/wacfrac image -c "$(bash ./scripts/palettes/$2.sh $3)" -f $(cat scripts/poi/$4) -r 3840 2160 -N 250 50 2 -z $5 -o $output_file.ppm
         ffmpeg -i $output_file.ppm $output_file.png
         rm $output_file.ppm
     fi
 }
 
-do_render center ultra 64 center 0.4
+do_render center ultra 24 center 0.4
 do_render toughreghnt seafoam 128 big_bang 1e10
 do_render ihmolayt fire 128 trees 1e10
 do_render wheb snow 64 Sx16 9e10
